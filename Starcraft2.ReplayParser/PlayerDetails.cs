250b1da2b5e8a2f32f32ded60c9942d05f0d5f4c
ascendedguard <ascendedguard@gmail.com>
2010-12-02 19:57:12 -0500

    Added documentation to the PlayerDetails class

--- Starcraft2.ReplayParser/PlayerDetails.cs
+++ Starcraft2.ReplayParser/PlayerDetails.cs
@@ -2,12 +2,34 @@
 
 namespace Starcraft2.ReplayParser
 {
+    /// <summary>
+    /// Class describing an individual player in a match.
+    /// </summary>
     public class PlayerDetails
     {
+        /// <summary>
+        /// Gets or sets the player's name.
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the player's race.
+        /// </summary>
         public string Race { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the type of player, whether he is human or computer. 
+        /// </summary>
         public PlayerType PlayerType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's color.
+        /// </summary>
         public Color Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's team number.
+        /// </summary>
         public int Team { get; set; }
     }
 }
