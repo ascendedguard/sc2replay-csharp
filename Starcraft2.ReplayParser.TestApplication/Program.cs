10690e332740c98068320b1067e78144f8b786d4
ascendedguard <ascendedguard@gmail.com>
2010-12-04 22:11:02 -0500

    Added simple test application

---
+++ Starcraft2.ReplayParser.TestApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Starcraft2.ReplayParser.TestApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            string appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            appPath = Path.GetDirectoryName(appPath);
+
+            string replayPath = Path.Combine(appPath, "testReplay.SC2Replay");
+
+            Replay replay = Replay.Parse(replayPath);
+
+            Console.Out.Write("Replay players: ");
+
+            foreach(var player in replay.Players)
+            {
+                Console.Out.Write(player.Name + " ");
+            }
+        }
+    }
+}
