cf60639e6a5b1909208b0011d628e66e97b2df6e
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:04:55 -0400

    Complete missing documentation

--- Starcraft2.ReplayParser/Difficulty.cs
+++ Starcraft2.ReplayParser/Difficulty.cs
@@ -1,16 +1,52 @@
-﻿namespace Starcraft2.ReplayParser
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Difficulty.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Describes the difficulty of a computer AI opponent.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starcraft2.ReplayParser
 {
     /// <summary>
-    /// Enumeration of available difficulties for computer players.
+    /// Describes the difficulty of a computer AI opponent.
     /// </summary>
     public enum Difficulty
     {
-        Unknown,
-        VeryEasy,
-        Easy,
-        Medium,
-        Hard,
-        VeryHard,
-        Insane,
+        /// <summary>
+        /// Unknown AI difficulty
+        /// </summary>
+        Unknown, 
+
+        /// <summary>
+        /// Very Easy AI difficulty
+        /// </summary>
+        VeryEasy, 
+
+        /// <summary>
+        /// Easy AI difficulty
+        /// </summary>
+        Easy, 
+
+        /// <summary>
+        /// Medium AI difficulty
+        /// </summary>
+        Medium, 
+
+        /// <summary>
+        /// Hard AI difficulty
+        /// </summary>
+        Hard, 
+
+        /// <summary>
+        /// Very Hard AI difficulty
+        /// </summary>
+        VeryHard, 
+
+        /// <summary>
+        /// Insane AI difficulty
+        /// </summary>
+        Insane, 
     }
 }
