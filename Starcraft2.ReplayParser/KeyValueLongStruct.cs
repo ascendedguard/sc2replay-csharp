44fb81227a60ee680f3036835b3bf1cf82e31b2c
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:34:15 -0400

    Renamed KeyLongValueStruct.cs to KeyValueLongStruct.cs to correctly match it's class'

---
+++ Starcraft2.ReplayParser/KeyValueLongStruct.cs
@@ -0,0 +1,106 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KeyLongValueStruct.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Represents a key-value pair in the replay format, with the value represented as a long.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starcraft2.ReplayParser
+{
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// Represents a key-value pair in the replay format.
+    /// </summary>
+    public struct KeyValueLongStruct
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets Key.
+        /// </summary>
+        public byte[] Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets Value.
+        /// </summary>
+        public long Value { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Parses a KeyValueStruct from an expected position in a BinaryReader,
+        ///   returning the KeyValueStruct and advancing the reader past the object.
+        /// </summary>
+        /// <param name="reader">
+        /// BinaryReader advanced to a point expecting a KeyValueStruct
+        /// </param>
+        /// <returns>
+        /// The KeyValueStruct found at the current reader position.
+        /// </returns>
+        public static KeyValueLongStruct Parse(BinaryReader reader)
+        {
+            var kv = new KeyValueLongStruct { Key = reader.ReadBytes(2), Value = ParseLongValueStruct(reader) };
+
+            return kv;
+        }
+
+        /// <summary>
+        /// The parse long value struct.
+        /// </summary>
+        /// <param name="reader">
+        /// The reader.
+        /// </param>
+        /// <returns>
+        /// The parse long value struct.
+        /// </returns>
+        public static long ParseLongValueStruct(BinaryReader reader)
+        {
+            long l2 = 0;
+
+            for (int k = 0;; k += 7)
+            {
+                long l1 = reader.ReadByte();
+
+                l2 |= (l1 & 0x7F) << k;
+
+                if ((l1 & 0x80) == 0)
+                {
+                    return (l2 & 1L) > 0L ? -(l2 >> 1) : l2 >> 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Overrides the ToString method, showing the Key and Value pairs in an appropriate format.
+        /// </summary>
+        /// <returns>
+        /// A string containing the Key in Hex format, with the value in decimal.
+        /// </returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("Key { ");
+
+            foreach (byte b in this.Key)
+            {
+                builder.AppendFormat("{0:X} ", b);
+            }
+
+            builder.Append("} - Value { ");
+
+            builder.AppendFormat("{0} ", this.Value);
+
+            builder.Append("}");
+            return builder.ToString();
+        }
+
+        #endregion
+    }
+}
