b2637dc7060bcf1c66819ce13cece810b2f8d2de
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:02:23 -0400

    Complete missing documentation

--- Starcraft2.ReplayParser/GameSpeed.cs
+++ Starcraft2.ReplayParser/GameSpeed.cs
@@ -1,12 +1,47 @@
-﻿namespace Starcraft2.ReplayParser
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameSpeed.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Describes the game speed a game was originally played at.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starcraft2.ReplayParser
 {
+    /// <summary>
+    /// Describes the game speed a game was originally played at.
+    /// </summary>
     public enum GameSpeed
     {
-        Unknown = 0,
-        Slower = 1,
-        Slow,
-        Normal,
-        Fast,
-        Faster,
+        /// <summary>
+        /// Unknown game speed.
+        /// </summary>
+        Unknown = 0, 
+
+        /// <summary>
+        /// Slower speed setting.
+        /// </summary>
+        Slower = 1, 
+
+        /// <summary>
+        /// Slow speed setting.
+        /// </summary>
+        Slow, 
+
+        /// <summary>
+        /// Normal game speed.
+        /// </summary>
+        Normal, 
+
+        /// <summary>
+        /// Fast speed setting.
+        /// </summary>
+        Fast, 
+
+        /// <summary>
+        /// Faster speed setting.
+        /// </summary>
+        Faster, 
     }
 }
--- Starcraft2.ReplayParser/GameType.cs
+++ Starcraft2.ReplayParser/GameType.cs
@@ -1,9 +1,32 @@
-﻿namespace Starcraft2.ReplayParser
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameType.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Describes the type of game played, whether on ladder or a custom game.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starcraft2.ReplayParser
 {
+    /// <summary>
+    /// Describes the type of game played, whether on ladder or a custom game.
+    /// </summary>
     public enum GameType
     {
-        Private,
-        Open,
+        /// <summary>
+        /// Custom game created by a player.
+        /// </summary>
+        Private, 
+
+        /// <summary>
+        /// Open player, played on the Battle.NET ladder system.
+        /// </summary>
+        Open, 
+
+        /// <summary>
+        /// Single Player campaign replay.
+        /// </summary>
         SinglePlayer
     }
 }
