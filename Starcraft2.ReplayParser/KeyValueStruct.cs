a6a852102bc86acb757ef8a03e7bfdf2e802a3eb
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:32:36 -0400

    Complete missing documentation

--- Starcraft2.ReplayParser/KeyValueStruct.cs
+++ Starcraft2.ReplayParser/KeyValueStruct.cs
@@ -1,57 +1,50 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KeyValueStruct.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Represents a key-value pair in the replay format.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Starcraft2.ReplayParser
 {
+    using System.IO;
+    using System.Text;
+
     /// <summary>
     /// Represents a key-value pair in the replay format.
     /// </summary>
     public struct KeyValueStruct
     {
+        #region Public Properties
+
+        /// <summary> Gets or sets the key, describing the data presented. </summary>
         public byte[] Key { get; set; }
+
+        /// <summary> Gets or sets the integer value of the structure. </summary>
         public int Value { get; set; }
 
-        /// <summary>
-        /// Overrides the ToString method, showing the Key and Value pairs in an appropriate format.
-        /// </summary>
-        /// <returns>A string containing the Key in Hex format, with the value in decimal.</returns>
-        public override string ToString()
-        {
-            var builder = new StringBuilder();
+        #endregion
 
-            builder.Append("Key { ");
-            
-            foreach(byte b in Key)
-            {
-                builder.AppendFormat("{0:X} ", b);                
-            }
-
-            builder.Append("} - Value { ");
-
-            builder.AppendFormat("{0} ", Value);
-
-            builder.Append("}");
-            return builder.ToString();
-        }
+        #region Public Methods
 
         /// <summary>
         /// Parses a KeyValueStruct from an expected position in a BinaryReader,
         /// returning the KeyValueStruct and advancing the reader past the object.
         /// </summary>
-        /// <param name="reader">BinaryReader advanced to a point expecting a KeyValueStruct</param>
-        /// <returns>The KeyValueStruct found at the current reader position.</returns>
+        /// <param name="reader"> BinaryReader advanced to a point expecting a KeyValueStruct </param>
+        /// <returns> The KeyValueStruct found at the current reader position. </returns>
         public static KeyValueStruct Parse(BinaryReader reader)
         {
-            var kv = new KeyValueStruct
-                {
-                    Key = reader.ReadBytes(2), 
-                    Value = ParseValueStruct(reader)
-                };
+            var kv = new KeyValueStruct { Key = reader.ReadBytes(2), Value = ParseValueStruct(reader) };
 
             return kv;
         }
 
+        /// <summary> Parses the value for a KeyValueStruct from the current position of a BinaryReader. </summary>
+        /// <param name="reader"> The reader, at the position of the KeyValueStruct's value object. </param>
+        /// <returns> The integer value of the structure. </returns>
         public static int ParseValueStruct(BinaryReader reader)
         {
             int j = 0;
@@ -67,5 +60,28 @@ namespace Starcraft2.ReplayParser
                 }
             }
         }
+
+        /// <summary> Overrides the ToString method, showing the Key and Value pairs in an appropriate format. </summary>
+        /// <returns> A string containing the Key in Hex format, with the value in decimal. </returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("Key { ");
+
+            foreach (byte b in this.Key)
+            {
+                builder.AppendFormat("{0:X} ", b);
+            }
+
+            builder.Append("} - Value { ");
+
+            builder.AppendFormat("{0} ", this.Value);
+
+            builder.Append("}");
+            return builder.ToString();
+        }
+
+        #endregion
     }
 }
