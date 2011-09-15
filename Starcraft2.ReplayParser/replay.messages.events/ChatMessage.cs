f0a5f886ad2275700381d48493c5a4aa8885de9e
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:38:59 -0400

    Filled in missing documentation

--- Starcraft2.ReplayParser/replay.messages.events/ChatMessage.cs
+++ Starcraft2.ReplayParser/replay.messages.events/ChatMessage.cs
@@ -1,9 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ChatMessage.cs" company="">
-//   
+// <copyright file="ChatMessage.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
 // </copyright>
 // <summary>
-//   Defines the ChatMessage type.
+//   Defines a single line of text in a replay's conversation log.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -11,16 +11,37 @@ namespace Starcraft2.ReplayParser
 {
     using System;
 
+    /// <summary>
+    /// Defines a single line of text in a replay's conversation log.
+    /// </summary>
     public class ChatMessage
     {
-        public TimeSpan Timestamp { get; set; }
-        public int PlayerId { get; set; }
-        public ChatMessageTarget MessageTarget { get; set; }
+        #region Public Properties
+
+        /// <summary> Gets or sets the chat message. </summary>
         public string Message { get; set; }
 
+        /// <summary> Gets or sets the target of the message. </summary>
+        public ChatMessageTarget MessageTarget { get; set; }
+
+        /// <summary> Gets or sets the Player Id who spoke the message. </summary>
+        public int PlayerId { get; set; }
+
+        /// <summary> Gets or sets the timestamp at which point the message was displayed. </summary>
+        public TimeSpan Timestamp { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary> Overrides the ToString method, to display the chat message similar to what's expected in-game. </summary>
+        /// <returns> Returns a chat message formatted similar to the in-game chat log. </returns>
         public override string ToString()
         {
-            return string.Format("({0}) [{1}] Player {2}: {3}", Timestamp, MessageTarget, PlayerId, Message);
+            return string.Format(
+                "({0}) [{1}] Player {2}: {3}", this.Timestamp, this.MessageTarget, this.PlayerId, this.Message);
         }
+
+        #endregion
     }
 }
