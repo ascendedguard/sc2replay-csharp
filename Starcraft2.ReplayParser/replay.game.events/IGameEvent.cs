348731fa7245f8a9ebcd8229e1486adba49eb6a0
Will Eddins <eddinsw@gmail.com>
2011-09-15 03:51:18 -0400

    Filled in missing documentation

--- Starcraft2.ReplayParser/replay.game.events/IGameEvent.cs
+++ Starcraft2.ReplayParser/replay.game.events/IGameEvent.cs
@@ -1,9 +1,28 @@
-﻿namespace Starcraft2.ReplayParser
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IGameEvent.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Interface for a basic game event.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Starcraft2.ReplayParser
 {
+    /// <summary> Interface for a basic game event. </summary>
     public interface IGameEvent
     {
+        #region Public Properties
+
+        /// <summary> Gets or sets the type of event. </summary>
         GameEventType EventType { get; set; }
+
+        /// <summary> Gets or sets the player performing the event. </summary>
         Player Player { get; set; }
+
+        /// <summary> Gets or sets the timestamp at which the event occured. </summary>
         Timestamp Time { get; set; }
+
+        #endregion
     }
 }
