eebe84de49d17c4eb3d4442b6c3c33872001280a
Will Eddins <eddinsw@gmail.com>
2011-09-15 04:07:14 -0400

    Filled in missing documentation

--- Starcraft2.ReplayParser/replay.game.events/HotkeyEvent.cs
+++ Starcraft2.ReplayParser/replay.game.events/HotkeyEvent.cs
@@ -1,24 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HotkeyEvent.cs" company="SC2ReplayParser">
+//   Copyright © 2011 All Rights Reserved
+// </copyright>
+// <summary>
+//   Describes an event where the player has set or used a hotkey.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Starcraft2.ReplayParser
 {
+    /// <summary> Describes an event where the player has set or used a hotkey. </summary>
     public class HotkeyEvent : GameEventBase
     {
+        #region Constructors and Destructors
+
+        /// <summary> Initializes a new instance of the <see cref="HotkeyEvent"/> class. </summary>
         public HotkeyEvent()
         {
             this.EventType = GameEventType.Other;
         }
 
-        public HotkeyEvent(Player player, Timestamp time) : base(player, time)
+        /// <summary> Initializes a new instance of the <see cref="HotkeyEvent"/> class. </summary>
+        /// <param name="player"> The player pressing the hotkey. </param>
+        /// <param name="time"> The time at which the event occured. </param>
+        public HotkeyEvent(Player player, Timestamp time)
+            : base(player, time)
         {
             this.EventType = GameEventType.Other;
         }
 
+        #endregion
+
+        #region Public Properties
+
+        /// <summary> Gets or sets the action performed. </summary>
+        public int Action { get; set; }
+
+        /// <summary> Gets or sets the pressed key. </summary>
         public int Key { get; set; }
 
-        public int Action { get; set; }
+        #endregion
     }
 }
